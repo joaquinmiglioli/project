5c55c3055f23e5fdf606852039c1f2e262d08f38
facso <facsonic43@gmail.com>
2025-11-04 12:01:48 -0300

    commit

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DBConnection {
 
    //Ajusten la contraseña a su propia base de datos
-    private static final String JDBC_URL  = "jdbc:postgresql://localhost:5432/monitoringcenter";
+    private static final String JDBC_URL  = "jdbc:postgresql://localhost:1525/monitoringcenter";
     private static final String USERNAME  = "postgres";
-    private static final String PASSWORD  = "mati123";
+    private static final String PASSWORD  = "Facundo-27";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
